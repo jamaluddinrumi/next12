9dc07ec9e0bedd906baa1dbbe0626d3544b684f0
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-02-11 19:44:57 +0700

    init

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+function About() {
+    return <div>Go to <Link href="/">Home Page</Link></div>
+}
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+function HomePage() {
+    return <div>Go to <Link href="/about">About</Link></div>
+}
+
+export default HomePage
