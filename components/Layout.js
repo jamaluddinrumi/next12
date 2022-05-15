a89c1e65f145163ef763dc318f2dcea2e54a3500
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-05-16 06:52:04 +0700

    add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" type="image/png" href="/favicon.png"></link>
+      </Head>
       <Header />
       <main className="container grid place-content-center h-screen">
         {children}
