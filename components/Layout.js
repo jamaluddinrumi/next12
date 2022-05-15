54f5430928900eda7523adbb5d5e5e181bf99d3f
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-05-16 06:55:04 +0700

    add footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Layout({ children }) {
@@ -7,10 +8,11 @@ export default function Layout({ children }) {
       <Head>
         <link rel="shortcut icon" type="image/png" href="/favicon.png"></link>
       </Head>
-      <Header />
+      <Header></Header>
       <main className="container grid place-content-center h-screen">
         {children}
       </main>
+      <Footer></Footer>
     </>
   );
 }
